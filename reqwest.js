597f8c3796957ca45029cc4f6a88195f5a4cf4b7
Piotr Bosak <piotr.hitori.bosak@gmail.com>
2015-09-23 10:39:07 +0200

    Commit to the rescue

---
+++ reqwest.js
@@ -0,0 +1,45 @@
+define([
+  'lib/qwest'
+],(
+  qwest
+)=>{
+  var cacheStash = {}
+
+  var defaultOptions = {
+    responseType: "json"
+  }
+
+  return {
+    call(url,data,options={}){
+      options = Object.merge(defaultOptions,options)
+      return new Promise((success,failure)=>{
+        (data
+          ? qwest.post(url,data,options)
+          : qwest.get(url,null,options)
+        )
+          .then(success)
+          .catch(failure)
+      })
+    },
+    callCache(url,data,maxAge=3600){
+      var key = JSON.stringify(arguments);
+      if(cacheStash[key]){
+        return cacheStash[key]
+      }else {
+        var localCacheOnly = Object.isString(maxAge);
+        setTimeout(()=>delete(cacheStash[key]),
+          localCacheOnly
+            ? 3600*1000
+            : maxAge*1000
+        )
+        return cacheStash[key] = this.call(url, data, (
+          localCacheOnly
+            ? {}
+            : {
+              cache: maxAge
+            }
+        ))
+      }
+    }
+  }
+})
